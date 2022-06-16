10486269c558041593eafbcb204963237c602f2b
owen <owen@noreply.git.ootn.xyz>
2022-06-16 07:23:10 -0500

    Update 'home/owen/misc/scripts/config_dwm.sh'

--- home/owen/misc/scripts/config_dwm.sh
+++ home/owen/misc/scripts/config_dwm.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd /home/owen/workspace/suckless/dwm-6.2 && sudo vim config.h && sudo make clean install
+cd $HOME/workspace/suckless/dwm-6.2 && sudo vim config.h && sudo make clean install
