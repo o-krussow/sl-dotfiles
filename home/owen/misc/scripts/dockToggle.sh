ed00621345c9f16efce9219b3abc72bb9e781c22
owen <owen@noreply.git.ootn.xyz>
2022-06-16 07:22:52 -0500

    Update 'home/owen/misc/scripts/dockToggle.sh'

--- home/owen/misc/scripts/dockToggle.sh
+++ home/owen/misc/scripts/dockToggle.sh
@@ -1,9 +1,9 @@
 #!/bin/bash                                                                                                                                                                                                                         
 #PUT THIS IN /etc/udev/rules.d/90-thinkpad-dock.rules
-#KERNEL=="card0", SUBSYSTEM=="drm", ENV{DISPLAY}=":0", ENV{XAUTHORITY}="/home/owen/.Xauthority", RUN+="/home/owen/misc/scripts/dockToggle.sh"
+#KERNEL=="card0", SUBSYSTEM=="drm", ENV{DISPLAY}=":0", ENV{XAUTHORITY}="$HOME/.Xauthority", RUN+="$HOME/misc/scripts/dockToggle.sh"
 
 export DISPLAY=:0
-export XAUTHORITY=/home/owen/.Xauthority
+export XAUTHORITY=$HOME/.Xauthority
 
 MONITOR='DP2-3'
 LAPTOP='eDP1'
@@ -15,13 +15,13 @@ if [[ -n $MONITOR_STATE ]]; then
     #xrandr --output $LAPTOP --off
     
     xrandr --output eDP1 --primary --mode 1366x768 --pos 0x282 --rotate normal --output DP1 --off --output DP2 --off --output DP2-1 --off --output DP2-2 --off --output DP2-3 --mode 1680x1050 --pos 1366x0 --rotate normal --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off
-    /home/owen/.fehbg
+    $HOME/.fehbg
 
 else
     #xrandr --output $MONITOR --off
     #xrandr --output $LAPTOP --auto
 
     xrandr --output eDP1 --primary --mode 1366x768 --pos 0x0 --rotate normal --output DP1 --off --output DP2 --off --output DP2-1 --off --output DP2-2 --off --output DP2-3 --off --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off
-    /home/owen/.fehbg
+    $HOME/.fehbg
 
 fi
